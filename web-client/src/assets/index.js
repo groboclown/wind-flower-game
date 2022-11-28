e2d83bb1c412be97d09468c203577a463c904ec0
groboclown <matt@groboclown.net>
2022-11-28 17:57:52 +0000

    Alternate base path experiment.

--- web-client/src/assets/index.js
+++ web-client/src/assets/index.js
@@ -5,5 +5,6 @@
 //   embedding in a relative URL, we need to do some
 //   work to find the location of the 'ammo' library.
 export function getAmmoLibraryRef() {
+  // TypeScript barfs on this; that's why this file is a .js not a .ts file.
   return `./${import.meta.env.BASE_URL}/ammo`
 }
