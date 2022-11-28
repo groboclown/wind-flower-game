38026e8c399dadf04cabae9079ec1d844abdc037
groboclown <matt@groboclown.net>
2022-11-28 18:01:20 +0000

    Another attempt at base path.

--- web-client/src/assets/index.js
+++ web-client/src/assets/index.js
@@ -5,6 +5,7 @@
 //   embedding in a relative URL, we need to do some
 //   work to find the location of the 'ammo' library.
 export function getAmmoLibraryRef() {
+  // See https://vitejs.dev/guide/build.html#public-base-path
   // TypeScript barfs on this; that's why this file is a .js not a .ts file.
   return `./${import.meta.env.BASE_URL}/ammo`
 }
