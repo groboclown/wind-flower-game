4b439b19e6249e5fe3fdfdc67a708acf0028831e
groboclown <matt@groboclown.net>
2022-12-16 16:16:31 +0000

    Generate token texture modes
    Rather than force each token type to include the hover/se/ect/etc modes, those are automatically generated off of the svg file.
    
    This also alters the uv-map format and the generated image to be a bit nicer, and have a better 0-1 resolution to better take advantage of the horizintal space.

--- web-client/src/assets/textures/mesh-texture/generate.sh
+++ web-client/src/assets/textures/mesh-texture/generate.sh
@@ -22,65 +22,122 @@ mkdir -p "$1" || exit 1
 outdir="$( cd "$1" && pwd )"
 cd "$( dirname "$0" )" || exit 1
 
+# This script generates the uv-map for every generated png file.
+# This is recorded in a json file that is carefully assembled
+# through simple 'echo' commands.  jq not required.
+
 echo '{"categories": {' > "${outdir}/uv-map.json"
 images=()
+rm_files=()
 current_height=0
-append=""
+row_append=""
+max_width=0
 for src in *.svg ; do
   if [ "${src:0:1}" != _ ] ; then
-    # Export this svg into a png.
     category="$( basename "${src}" .svg )"
-    outname="${outdir}/${category}.png"
-    inkscape \
-      --export-filename="${outname}" \
-      --export-overwrite \
-      --export-type=png \
-      --export-width=1000 \
-      --export-height=866 \
-      "${src}" || exit 1
+    echo "Generating ${category}..."
 
-    # Record it.
-    images+=("${outname}")
+    category_images=()
+
+    # Assemble the decal variations on a single row.
+    current_width=0
+
     h0=${current_height}
     h1=$(( current_height + 432 ))
     h2=$(( current_height + 433 ))
     h3=$(( current_height + 865 ))
     current_height=$(( current_height + 866 ))
-    echo "${append}\"${category}\": [" >> "${outdir}/uv-map.json"
 
-    # triangle 0
-    # echo "[[0, ${h1}], [499, ${h1}], [249, ${h0}]], " >> "${outdir}/uv-map.json"
-    echo "[[249, ${h0}], [0, ${h1}], [499, ${h1}]], " >> "${outdir}/uv-map.json"
+    echo "${row_append}\"${category}\": {" >> "${outdir}/uv-map.json"
+    row_append=", "
+    cat_append=""
 
-    # triangle 1
-    # echo "[[749, ${h0}], [250, ${h0}], [500, ${h1}]], " >> "${outdir}/uv-map.json"
-    echo "[[500, ${h1}], [749, ${h0}], [250, ${h0}]], " >> "${outdir}/uv-map.json"
 
-    # triangle 2
-    # echo "[[500, ${h1}], [999, ${h1}], [749, ${h0}]], " >> "${outdir}/uv-map.json"
-    echo "[[749, ${h0}], [500, ${h1}], [999, ${h1}]], " >> "${outdir}/uv-map.json"
+    # This is a category + variation for a tile.
+    # Each SVG file must wrap its contents inside a <g> tag, so that
+    # the last text elements are '</g>(\n|\r)*</svg>'
+    # This allows the SVG to append the selection decorations.
+    #
+    # This svg is then converted to a png, along with the different
+    # decorations, and all of them are put onto a single row of the final texture map.
+    for mode in "normal" "hover" "select" "hover_select" ; do
+      genfile="${outdir}/${category}-${mode}.svg"
+      outname="${outdir}/${category}-${mode}.png"
 
-    # triangle 3
-    # echo "[[499, ${h2}], [0, ${h2}], [249, ${h3}]], " >> "${outdir}/uv-map.json"
-    echo "[[249, ${h3}], [499, ${h2}], [0, ${h2}]], " >> "${outdir}/uv-map.json"
+      # Strip off the '</g></svg>' from the genfile.  Note that '<svg>' should
+      # only appear at the start, and thus '</svg>' can only appear at the end.
+      # Because any amount of whitespace can appear between tags, it must be
+      # removed.  This has the side-effect of removing spaces in text, but a tile
+      # shouldn't include text elements.
+      cat "${src}" \
+        | tr -d '\r\n' | tr -s '[:space:]' \
+        | sed 's|</g></svg>||' | sed 's|</g> </svg>||' \
+        > "${genfile}.1"
 
-    # triangle 4
-    # echo "[[250, ${h3}], [749, ${h3}], [499, ${h2}]], " >> "${outdir}/uv-map.json"
-    echo "[[499, ${h2}], [250, ${h3}], [749, ${h3}]], " >> "${outdir}/uv-map.json"
+      # Now stick on the asset.
+      cat "${genfile}.1" "_${mode}.svg.fragment" > "${genfile}"
 
-    # triangle 5
-    # echo "[[999, ${h2}], [500, ${h2}], [749, ${h3}]]]" >> "${outdir}/uv-map.json"
-    echo "[[749, ${h3}], [999, ${h2}], [500, ${h2}]]]" >> "${outdir}/uv-map.json"
+      inkscape \
+        --export-filename="${outname}" \
+        --export-overwrite \
+        --export-type=png \
+        --export-width=1000 \
+        --export-height=866 \
+        "${genfile}" || exit 1
 
-    append=", "
+      category_images+=("${outname}")
+      rm_files+=("${outname}" "${genfile}.1" "${genfile}")
+
+      echo "${cat_append}\"${mode}\": [" >> "${outdir}/uv-map.json"
+      cat_append=", "
+
+      w0=$(( current_width + 0 ))
+      w249=$(( current_width + 249 ))
+      w250=$(( current_width + 250 ))
+      w499=$(( current_width + 499 ))
+      w500=$(( current_width + 500 ))
+      w749=$(( current_width + 749 ))
+      w999=$(( current_width + 999 ))
+      current_width=$(( current_width + 1000 ))
+      if [ ${current_width} -gt ${max_width} ] ; then
+        max_width=${current_width}
+      fi
+
+      # triangle 0
+      echo "[[${w249}, ${h0}], [${w0}, ${h1}], [${w499}, ${h1}]], " >> "${outdir}/uv-map.json"
+
+      # triangle 1
+      echo "[[${w500}, ${h1}], [${w749}, ${h0}], [${w250}, ${h0}]], " >> "${outdir}/uv-map.json"
+
+      # triangle 2
+      echo "[[${w749}, ${h0}], [${w500}, ${h1}], [${w999}, ${h1}]], " >> "${outdir}/uv-map.json"
+
+      # triangle 3
+      echo "[[${w249}, ${h3}], [${w499}, ${h2}], [${w0}, ${h2}]], " >> "${outdir}/uv-map.json"
+
+      # triangle 4
+      echo "[[${w499}, ${h2}], [${w250}, ${h3}], [${w749}, ${h3}]], " >> "${outdir}/uv-map.json"
+
+      # triangle 5
+      echo "[[${w749}, ${h3}], [${w999}, ${h2}], [${w500}, ${h2}]]]" >> "${outdir}/uv-map.json"
+    done
+    echo "}" >> "${outdir}/uv-map.json"
+
+    # Create a horizontally appended stack of images.
+    convert +append "${category_images[@]}" "${outdir}/_texture-${category}.png" || exit 1
+    images+=("${outdir}/_texture-${category}.png")
+    rm_files+=("${outdir}/_texture-${category}.png")
+
   fi
 done
-echo "}, \"width\": 1000, \"height\": ${current_height}}" >> "${outdir}/uv-map.json"
+echo "}, \"width\": ${max_width}, \"height\": ${current_height} }" >> "${outdir}/uv-map.json"
 
 # Create a vertically appended stack of images.
+echo "Generating the final image..."
 convert -append "${images[@]}" "${outdir}/mesh-texture.png" || exit 1
 
 # Don't need to keep the intermediate png images.  They should be deleted.
-for img in "${images[@]}" ; do
+echo "Cleaning up..."
+for img in "${rm_files[@]}" ; do
   rm "${img}"
 done
--- web-client/src/scenes/gameboard/texture-handler.ts
+++ web-client/src/scenes/gameboard/texture-handler.ts
@@ -19,79 +19,88 @@ interface CategoryMap {
   [key: string]: CategoryTexture
 }
 
-
+// hard-coded asset name
 const UNKNOWN_CATEGORY = 'unknown'
+
+interface JsonCategoryVariation {
+  normal: number[][][]
+  hover: number[][][]
+  select: number[][][]
+  hover_select: number[][][]
+}
+
+interface ModeLookup {
+  jsonKey: keyof JsonCategoryVariation
+  catKey: keyof CategoryTexture
+}
+
+const MODES: ModeLookup[] = [
+  {jsonKey: "normal", catKey: "normal"},
+  {jsonKey: "hover", catKey: "hover"},
+  {jsonKey: "select", catKey: "select"},
+  {jsonKey: "hover_select", catKey: "hoverSelect"},
+]
 
 
 export class TextureHandler {
   private categories: CategoryMap
 
   constructor(uvMap: JSONValueType) {
+    const self = this
+    // Texture UV map format:
+    //   { "categories": {
+    //        "(category name)": {
+    //            "normal|hover|select|hover-select": [
+    //                   // one per triangle, so 6 of these.
+    //                   [[u, v], [u, v], [u, v]],
+    //            ]
+    //        }
+    //     }, "width": (image width), "height": (image height)
+    //   }
+
     if (uvMap === null || Array.isArray(uvMap) || typeof uvMap !== 'object') {
       throw new Error('bad uv-map.json format')
     }
     const texelWidth = uvMap.width as number
     const texelHeight = uvMap.height as number
-    const uvCategories = uvMap.categories as {[key: string]: number[][][]}
+    const uvModeCategories = uvMap.categories as {[key: string]: any}
 
     this.categories = {}
+
     Object.keys(uvMap.categories as object).forEach((mapName: string) => {
       let resName = mapName
-      let hover = false
-      let select = false
-      if (resName.endsWith(".hover")) {
-        hover = true
-        resName = resName.substring(0, resName.lastIndexOf('.'))
-      } else if (resName.endsWith(".select")) {
-        select = true
-        resName = resName.substring(0, resName.lastIndexOf('.'))
-      } else if (resName.endsWith(".hover-select")) {
-        hover = true
-        select = true
-        resName = resName.substring(0, resName.lastIndexOf('.'))
-      }
-      let cat = resName
-      const pPos = cat.lastIndexOf('.')
+
+      let catName = resName
+      const pPos = catName.lastIndexOf('.')
       if (pPos > 0) {
         // We don't care about the actual index.  It's just to make them separate.
-        cat = cat.substring(0, pPos)
+        catName = catName.substring(0, pPos)
       }
+      let cat = self.categories[catName]
+      if (cat === undefined) {
+        cat = {normal: [], hover: [], select: [], hoverSelect: []}
+        self.categories[catName] = cat
+      }
+      const uvModes = uvModeCategories[mapName] as JsonCategoryVariation
 
-      // Map each UV value in the source (which is in texels) to the (0, 1)
-      // required by the texture mapping.
-      const uv: number[][][] = []
-      const mapping = uvCategories[mapName]
-      for (let faceI = 0; faceI < 6; faceI++) {
-        const face: number[][] = []
-        uv.push(face)
-        for (let vertexI = 0; vertexI < 3; vertexI++) {
-          face.push([
-            (mapping[faceI][vertexI][0]) / texelWidth,
-            1 - ((mapping[faceI][vertexI][1]) / texelHeight),
-          ])
+      MODES.forEach((mode) => {
+        // Map each UV value in the source (which is in texels) to the (0, 1)
+        // required by the texture mapping.
+        const uv: number[][][] = []
+        const srcMapping = uvModes[mode.jsonKey]
+        for (let faceI = 0; faceI < 6; faceI++) {
+          const face: number[][] = []
+          uv.push(face)
+          for (let vertexI = 0; vertexI < 3; vertexI++) {
+            face.push([
+              (srcMapping[faceI][vertexI][0]) / texelWidth,
+              1 - ((srcMapping[faceI][vertexI][1]) / texelHeight),
+            ])
+          }
         }
-      }
-      // console.debug(`${mapName} (${cat})[0][0] = ${mapping[0][0][0]}, ${mapping[0][0][1]}`)
-
-      let tex: CategoryTexture | undefined = this.categories[cat]
-      if (tex === undefined) {
-        tex = {
-          normal: [],
-          hover: [],
-          select: [],
-          hoverSelect: [],
-        }
-        this.categories[cat] = tex
-      }
-      if (hover && select) {
-        tex.hoverSelect.push({ uv })
-      } else if (hover) {
-        tex.hover.push({ uv })
-      } else if (select) {
-        tex.select.push({ uv })
-      } else {
-        tex.normal.push({ uv })
-      }
+        // console.debug(`${mapName} (${cat})[0][0] = ${mapping[0][0][0]}, ${mapping[0][0][1]}`)
+        cat[mode.catKey].push({ uv })
+      })
     })
 
     console.log(`Loaded texture categories ${Object.keys(this.categories)}`)
